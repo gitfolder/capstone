145ea46303450a4b3b968069fcadd2f71bec88af
gitfolder <02newfolder@gmail.com>
2022-08-19 14:48:45 +0100

    Final Upload
    Final Changes and Final Versions Uploaded

--- Code/CNN/util_functions.py
+++ Code/CNN/util_functions.py
@@ -9,7 +9,6 @@ import tensorflow as tf
 from tensorflow.keras import layers
 
 from keras import backend as K
-#from keras import callbacks
 
 from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
@@ -22,29 +21,40 @@ from keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.utils import to_categorical 
 
 from io import StringIO
-#from tqdm import tqdm
 
-
-
+# Learning Rate Decay 
 decay_rate = 0.1 # 10%
 
-# Logarithmic decay function for learning rate
+
 def exp_decay(epoch: int, learning_rate: float) -> float:
+    '''
+    Logarithmic decay function for learning rate
+    Arguments:
+        epoch: current epoch
+        learning_rate: current learning rate
+    Returns:
+        adjusted learning rate
+    '''
     lrate = learning_rate * np.exp(-decay_rate*epoch)
     return lrate
 
 def compile_model(num_classes: int=7, img_size: tuple=(224,224)) -> Sequential:
-    
-    #dropout_rate = 0.3
+    '''
+    This function compiles Basic CNN (Sequential) model
+    Arguments:
+        num_classes: number of classes to predict
+        img_size: input size
+    Returns:
+        Sequential model
+    '''
     regularization = 0.01 # L1
     learning_rate = 0.00025
     epsilon = 1e-06
     
     activation = activations.relu
-    #weight_init = tf.keras.initializers.HeNormal()
     
     padding = "valid"
-    # Rescaling is Standartization of the data, this layer will be used with colors 
+    # Rescaling is Standartization of the data, this layer will be used with color channels 
     # to convert values from 0-255 into 0-1 range. This step is done to normalize data 
     # for Neural Network, key is to have mean value as close to 0 as possible, 
     # this will speed up learning and will provide faster convergence.
@@ -54,58 +64,51 @@ def compile_model(num_classes: int=7, img_size: tuple=(224,224)) -> Sequential:
         layers.Rescaling(1./255, input_shape=img_size+(3,)),
          
         layers.Conv2D(16, (3,3), padding=padding, activation=activation,
-                      kernel_regularizer=keras.regularizers.l1(regularization)),
-                     #kernel_initializer=weight_init),
+                      kernel_regularizer=keras.regularizers.l1(regularization)),              
         layers.MaxPooling2D(),
         
         layers.Conv2D(32, (3,3), padding=padding, activation=activation,
                       kernel_regularizer=keras.regularizers.l1(regularization)),
-                     #kernel_initializer=weight_init),
         layers.MaxPooling2D(),
-        
-        #layers.Dropout(dropout_rate),
         
         layers.Conv2D(64, (3,3), padding=padding, activation=activation,
                       kernel_regularizer=keras.regularizers.l1(regularization)),
-                     #kernel_initializer=weight_init),
         layers.MaxPooling2D(),
         
-        #layers.Dropout(dropout_rate),
-        #layers.BatchNormalization(), # momentum=0.8 
-        
         layers.Flatten(),
-        
-        #layers.GlobalAveragePooling2D(),
-    
-        #layers.Dropout(rate=dropout_rate),
-        #layers.BatchNormalization(),
-
-        layers.Dense(512, activation="relu"), #, kernel_initializer=weight_init),
-        #layers.Dropout(rate=dropout_rate),
+        layers.Dense(512, activation="relu"),
         layers.Dense(num_classes, activation="softmax") # default linear activation
     ])
     
     # Try focal loss for uneven class distribution
     # Adagrad - Adaptive Gradient
     optimizer = keras.optimizers.Adam(learning_rate=learning_rate, epsilon=epsilon)     
-    
-    #import functools 
 
-    #top3acc = functools.partial(keras.metrics.top_k_categorical_accuracy, k=3)
-    #top3acc.__name__ = "top3acc"
-    
     # CategoricalCrossentropy is used because labels are OneHotEncoder format
     model.compile(optimizer=optimizer,
                   loss="categorical_crossentropy",
                   metrics=["accuracy"]) #, "top_k_categorical_accuracy", top3acc])
     return model
 
-## Mish Activation Function
-def mish(x):
+def mish(x: float) -> tf.Tensor:
+    '''
+    Mish activation function (as it was not part of keras package)
+    Arguments:
+        x: activation function input
+    Returns:
+        TensorFlow Tensor
+    '''
     return keras.layers.Lambda(lambda x: x*K.tanh(K.softplus(x)))(x)
 
-def print_data_distribution(iter_data: keras.preprocessing.image.DirectoryIterator):
-    
+def print_data_distribution(iter_data: keras.preprocessing.image.DirectoryIterator) -> None:
+    '''
+    This function prints data distribution of iterator
+    Was used in early stages of development to ensure correct data distribution 
+    Arguments:
+        iter_data: iterator
+    Returns:
+        None
+    '''
     # combine data from iterator
     labels_all = iter_data.labels
     n_samples = len(labels_all)   # sum(1 for _ in iter)
@@ -118,14 +121,26 @@ def print_data_distribution(iter_data: keras.preprocessing.image.DirectoryIterat
         perc = round(count/n_samples*100,2)
         print("{:6}{:8}{:8}%".format(index, count, perc))
 
-
-
 def fit_model(model: Sequential, train_ds: Dataset, test_ds: Dataset, 
               epochs: int=10, augment: str="no_augment", verbose: bool=True) -> None:
-    
-    #trigger = callbacks.EarlyStopping(monitor="val_accuracy", mode="max", min_delta=0.001,
-    #                                  patience=10, restore_best_weights=True)
     '''
+    This function trains the model with training data
+    Arguments:
+        model: Squential model to be trained
+        train_ds: Dataset to use as training data
+        test_ds: Dataset to use as testing data for training
+        epochs: length of training, number of epochs
+        augment: string to indicate an augmentation to add to model name on save
+        verbose: evaluate and print training history
+    Returns:
+        None
+    '''
+    
+    # These are various options used in testing, we decided to leave them here
+    '''
+    trigger = callbacks.EarlyStopping(monitor="val_accuracy", mode="max", min_delta=0.001,
+                                      patience=10, restore_best_weights=True)
+    
     early_stop = callbacks.EarlyStopping(monitor="val_loss", mode="min", min_delta=0.01,
                                       patience=5, restore_best_weights=True)
     save_weights = callbacks.ModelCheckpoint(
@@ -136,8 +151,6 @@ def fit_model(model: Sequential, train_ds: Dataset, test_ds: Dataset,
     ''';
     
     history = model.fit(train_ds, validation_data=test_ds, epochs=epochs)
-                       #callbacks=[lr_sheduler]) 
-                        #callbacks=[early_stop, save_weights])
     epochs = len(history.history['loss'])
     print("Epoch Length        : {0}".format(epochs))
     
@@ -148,12 +161,29 @@ def fit_model(model: Sequential, train_ds: Dataset, test_ds: Dataset,
     model.save("output\keras_models\skin_classifier_{0}_{1}.keras".format(epochs, augment))
 
 def evaluate_model(model: Sequential, train_ds: Dataset, test_ds: Dataset) -> None:
+    '''
+    Evaluate Sequential model based on training and testing data and print results
+    Arguments:
+        model: Sequential model to evaluate
+        train_ds: Dataset to use as training data
+        test_ds: Dataset to use as testing data for training        
+    Returns:
+        None
+    '''
     acc_value = model.evaluate(train_ds, verbose=0)[1]
     val_acc_value = model.evaluate(test_ds, verbose=0)[1]
     print("Model Accuracy      : {0:.4f}".format(acc_value))
     print("Validation Accuracy : {0:.4f}".format(val_acc_value))
     
 def print_training_history(history: History) -> None:
+    '''
+    This functon prints training history graph
+    Loss and Validation Loss side-by-side with Accuracy and Validation Accuracy
+    Arguments:
+        history: model training history (fit function output)
+    Returns:
+        None
+    '''
     acc = history.history["accuracy"]
     val_acc = history.history["val_accuracy"]
     
@@ -182,8 +212,19 @@ def print_training_history(history: History) -> None:
     plt.show()
     
 def print_filters(model: Sequential, plot_weights: bool=False, 
-                  test_img_path: str="input/ISIC_0024891.jpg") -> None:
-    global IMG_SIZE
+                  test_img_path: str="input/ISIC_0024891.jpg", 
+                  img_size: tuple=(224,224)) -> None:
+    '''
+    This function prints convolutional filters of a model 
+    with or without weights based on input image
+    Arguments:
+        model: Sequential model to print filters of
+        plot_weights: wheather to print weights or not
+        test_img_path: image path to be used to print filters and weights
+        img_size: input shape of a model to convert image to
+    Returns:
+        None
+    '''
     # Dynamically get convolution layer indexes from model
     layer_indx = [i for i,layer in enumerate(model.layers) if "Conv" in str(layer)]
 
@@ -196,7 +237,7 @@ def print_filters(model: Sequential, plot_weights: bool=False,
         model_t = keras.Model(inputs=model.inputs, outputs=conv_layer.output)
 
         # Convert image to array
-        img = load_img(test_img_path, target_size=IMG_SIZE)
+        img = load_img(test_img_path, target_size=img_size)
         img = img_to_array(img)
         img = np.expand_dims(img, axis=0)
 
@@ -207,7 +248,7 @@ def print_filters(model: Sequential, plot_weights: bool=False,
         rows = int(conv_layer.filters/cols)
 
         for i in range(rows*cols):
-            ax = plt.subplot(cols, cols, i+1)
+            ax = plt.subplot(rows, cols, i+1)
             ax.set_xticks([])
             ax.set_yticks([])
             # plot filter channel in grayscale
@@ -220,7 +261,7 @@ def print_filters(model: Sequential, plot_weights: bool=False,
         if plot_weights:
             x1w = conv_layer.get_weights()[0][:,:,0,:]
             for i in range(0,conv_layer.filters):
-                ax = plt.subplot(cols, cols, i+1)
+                ax = plt.subplot(rows, cols, i+1)
                 ax.set_xticks([])
                 ax.set_yticks([])
                 plt.imshow(x1w[:,:,i], interpolation="nearest", cmap="gray")
@@ -231,7 +272,19 @@ def print_confusion_matrix(model: Sequential, val_image_path: str,
                            supress_print: bool=False,
                            data: Dataset=None
                            ) -> pd.DataFrame:
-
+    '''
+    This function print confusion matrix (CM) for a Sequential model
+    Substituted with ROC and Presision-Recall graphs at later stages
+    Arguments:
+        model: Sequential model to print CM
+        val_image_path: path for validation image folder
+        normalize: whether to normalize CM or not (% or float)
+        img_size: image shape
+        supress_print: supressing printing of a matrix to speed-up return
+        data: validation data can be passed as dataset if desired
+    Returns:
+        Pandas Dataframe with metrics 
+    '''
     # Get validation images
     batch_size = 64
     
@@ -264,9 +317,7 @@ def print_confusion_matrix(model: Sequential, val_image_path: str,
 
         # Here Recall is Sensitivity
         print("Accuracy Score : ", round(accuracy_score(y, y_pred_int),4))
-    # Replaced by DataFrame with added Specificity
-    #print("Classification Report :\n", classification_report(y, y_pred_int))
-
+    
     # Get data from classification report and parse it
     cr = classification_report(y, y_pred_int, digits=4, zero_division=0)
     cr = cr[:cr.index("accuracy")]
@@ -306,6 +357,15 @@ def print_confusion_matrix(model: Sequential, val_image_path: str,
 def get_top_k_accuracy(Y_real: np.ndarray,
                        Y_predicted: np.ndarray,
                        k: int=3) -> float:
+    '''
+    This function calculates Top-K accuracy
+    Arguments:
+        Y_real: truth labels
+        Y_predicted: predicted labels
+        k: Top-K accuracy integer (3 for Top-3 accuracy)
+    Returns:
+        Top-K accuracy (float)
+    '''
     metrics = tf.keras.metrics.top_k_categorical_accuracy(
         Y_real, Y_predicted, k=k)
     _, counts = np.unique(metrics, return_counts=True)
@@ -316,6 +376,15 @@ def get_top_k_accuracy(Y_real: np.ndarray,
 def print_top_k_accuracy(keras_model: Sequential, 
                          data: keras.preprocessing.image.DirectoryIterator,
                          k: int=5) -> None:
+    '''
+    This function prints Top-K accuracy
+    Arguments:
+        keras_model: Sequential model to print accuracy of
+        data: iterator with data to base accuracy upon
+        k: Top-K accuracy integer (3 for Top-3 accuracy)
+    Returns:
+        None
+    '''
     # OneHotEncoder convertion 
     y_real = to_categorical(data.labels)
     y_pred = keras_model.predict(data)
